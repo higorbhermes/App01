47ae554fc1bed6cd0e0fa059a40accd7f2f56317
higorbhermes <higorbhermes@gmail.com>
2017-10-23 19:14:28 -0200

    Inicio do Projeto

---
+++ app/src/main/java/pooa20172/iff/br/app01/TelaPrincipal.java
@@ -0,0 +1,13 @@
+package pooa20172.iff.br.app01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TelaPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tela_principal);
+    }
+}
